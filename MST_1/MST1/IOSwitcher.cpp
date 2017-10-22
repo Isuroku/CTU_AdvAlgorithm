25a404800afa8a6b6615feb66fa48dd6bbaeb1a2
Andrey Ekimov <ekimov@quantgames.com>
2017-10-22 13:24:31 +0300

    getline3

--- MST_1/MST1/IOSwitcher.cpp
+++ MST_1/MST1/IOSwitcher.cpp
@@ -14,7 +14,7 @@ CIOSwitcher::CIOSwitcher(const bool inFromFile, const string inFileName): _from_
 bool CIOSwitcher::getline(string& out_string) const
 {
 	char tmp[128];
-	memset(&tmp, 0, sizeof tmp);
+	memset(&tmp, 0, sizeof(tmp));
 	_pfile_stream->getline(tmp, 128);
 
 	out_string.assign(tmp);
