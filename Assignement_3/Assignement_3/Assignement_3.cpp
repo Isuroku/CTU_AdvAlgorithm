cda6f650a97907d0e06fcc291a6a8d96dcc779b5
Andrey Ekimov <ekimov@quantgames.com>
2017-11-11 22:40:42 +0300

    на первом тесте отладил рассчет сертификата

--- Assignement_3/Assignement_3/Assignement_3.cpp
+++ Assignement_3/Assignement_3/Assignement_3.cpp
@@ -5,6 +5,7 @@
 #include "Vertex.h"
 #include "GraphSaver.h"
 #include <sstream>
+#include <algorithm>
 
 const string arr_file_names[] =
 {
@@ -20,9 +21,7 @@ const string arr_file_names[] =
 	"Pd/pub10.in", //131282
 };
 
-
-
-int main()
+bool ReadSources(const bool inSaveGraphToFile, vector<vector<CVertex>>& outAllData)
 {
 	const int test_n = 0;
 	CIOSwitcher IOSwitcher(true, arr_file_names[test_n]);
@@ -34,16 +33,16 @@ int main()
 	if (sscanf(line.c_str(), "%zu %zu %zu", &vertex_count, &edge_count, &molecule_count) != 3)
 	{
 		cerr << "read first line was wrong!";
-		return 1;
+		return false;
 	}
 
-	vector<vector<CVertex>> molecules(molecule_count);
+	outAllData.resize(molecule_count);
 
 	CGraphSaver sg;
-	
-	for(size_t m = 0; m < molecule_count; m++)
+
+	for (size_t m = 0; m < molecule_count; m++)
 	{
-		vector<CVertex> mol_vec = molecules[m];
+		vector<CVertex>& mol_vec = outAllData[m];
 		mol_vec.resize(vertex_count);
 
 		for (size_t e = 0; e < edge_count; e++)
@@ -54,7 +53,7 @@ int main()
 			if (sscanf(line.c_str(), "%zu %zu", &ind1, &ind2) != 2)
 			{
 				cerr << "read " << m * molecule_count + e + 1 << " line was wrong!";
-				return 1;
+				return false;
 			}
 
 			CVertex& v1 = mol_vec[ind1 - 1];
@@ -65,12 +64,141 @@ int main()
 			v1.neighbours.push_back(&v2);
 			v2.neighbours.push_back(&v1);
 		}
-		
-		stringstream lbl;
-		lbl << "m" << m << ".grafa";
-		sg.save_txt_file(mol_vec, lbl.str());
+
+		if (inSaveGraphToFile)
+		{
+			stringstream lbl;
+			lbl << "m" << m << ".grafa";
+			sg.save_txt_file(mol_vec, lbl.str());
+		}
 	}
+	return true;
+}
+
+struct SMolClasses
+{
+	string Cert;
+	size_t Count;
+
+	SMolClasses(): Cert(""), Count(0) {	}
+	SMolClasses(const string& inCert) : Cert(inCert), Count(1) {	}
+
+	static bool cmp(const SMolClasses& elem1, const SMolClasses& elem2)
+	{
+		return elem1.Count < elem2.Count;
+	}
+};
+
+
+
+struct SFindRes
+{
+	string cert;
+
+	SFindRes(const string& incert): cert(incert) {}
+
+	bool operator()(const SMolClasses& other) const
+	{
+		return strcmp(cert.c_str(), other.Cert.c_str()) == 0;
+	}
+};
+
+void CollectChilds(vector<CVertex>& in_vertices, vector<vector<CVertex*>>& out_childs, vector<size_t>& out_indices)
+{
+	for_each(in_vertices.begin(), in_vertices.end(), [&](CVertex& v)
+	{
+		size_t sz = v.neighbours.size();
+		if (sz != 1)
+			return;
+
+		size_t index = v.neighbours[0]->vec_index();
+		out_childs[index].push_back(&v);
+
+		vector<size_t>::iterator it = lower_bound(out_indices.begin(), out_indices.end(), index);
+		if(it == out_indices.end() || *it != index)
+			out_indices.insert(it, index);
+	});
+}
+
+void MergeChilds(vector<size_t>& indices, vector<CVertex>& in_vertices, vector<vector<CVertex*>>& io_childs)
+{
+	for_each(indices.begin(), indices.end(), [&](size_t index)
+	{
+		vector<CVertex*>& childs = io_childs[index];
+		CVertex& parent = in_vertices[index];
+		parent.MergeCert(childs);
+		childs.clear();
+	});
+}
+
+void InsertNewCert(const string& inCert, vector<SMolClasses>& outResult)
+{
+	const vector<SMolClasses>::iterator it = find_if(outResult.begin(), outResult.end(), SFindRes(inCert));
+	if (outResult.end() != it)
+		it->Count++;
+	else
+		outResult.push_back(SMolClasses(inCert));
+}
+
+int main()
+{
+	vector<vector<CVertex>> molecules;
+	if (!ReadSources(false, molecules))
+		return 1;
+
+	vector<SMolClasses> result;
+
+	//vector<CVertex*> tree1;
+	//vector<CVertex*> tree2;
+
+	vector<size_t> indices;
+	vector<vector<CVertex*>> childs;
+
+	for_each(molecules.begin(), molecules.end(), [&](vector<CVertex>& mol)
+	{
+		childs.resize(mol.size());
+		indices.clear();
+
+		CollectChilds(mol, childs, indices);
+
+		while(indices.size() > 2)
+		{
+			MergeChilds(indices, mol, childs);
+
+			indices.clear();
+			CollectChilds(mol, childs, indices);
+		}
+
+		MergeChilds(indices, mol, childs);
+
+		string cert;
+		if(indices.size() == 1)
+		{
+			cert = mol[indices[0]].cert;
+		}
+		else
+		{
+			stringstream lbl;
+			string& ch_cert1 = mol[indices[0]].cert;
+			string& ch_cert2 = mol[indices[1]].cert;
+			if (CVertex::icmp(&ch_cert1, &ch_cert2))
+				lbl << ch_cert1 << ch_cert2;
+			else
+				lbl << ch_cert2 << ch_cert1;
+			cert = lbl.str();
+		}
+
+		InsertNewCert(cert, result);
+	});
+
+	sort(result.begin(), result.end(), SMolClasses::cmp);
+
+	cout << result[0].Count;
+	for(size_t i = 1; i < result.size(); ++i)
+		cout << " " << result[i].Count;
 
     return 0;
 }
+
+
 
--- Assignement_3/Assignement_3/Vertex.h
+++ Assignement_3/Assignement_3/Vertex.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -9,7 +11,9 @@ class CVertex
 public:
 	vector<CVertex*> neighbours;
 
-	CVertex() : _id(0)
+	string cert;
+
+	CVertex() : cert("ab"), _id(0)
 	{
 	}
 
@@ -19,6 +23,47 @@ public:
 	size_t vec_index() const { return _id - 1; }
 
 	string get_debuf_info() const { return ""; }
+
+	bool is_leaf() const { return neighbours.size() == 1; };
+
+	static bool icmp(const string* elem1, const string* elem2)
+	{
+		return _stricmp(elem1->c_str(), elem2->c_str()) < 0;
+	}
+
+	void MergeCert(vector<CVertex*>& childs)
+	{
+		vector<string*> crts;
+		crts.reserve(childs.size() + 1);
+
+		string part;
+		if (cert.length() > 2)
+		{
+			part = cert.substr(1, cert.length() - 2);
+			crts.push_back(&part);
+		}
+
+		for (size_t j = neighbours.size() - 1; j != size_t(-1); j--)
+		{
+			CVertex* n = neighbours[j];
+			if (count(childs.begin(), childs.end(), n) > 0)
+			{
+				vector<string*>::iterator it = lower_bound(crts.begin(), crts.end(), &n->cert, icmp);
+				crts.insert(it, &n->cert);
+
+				n->neighbours.clear();
+
+				neighbours.erase(neighbours.begin() + j);
+			}
+		}
+
+		stringstream lbl;
+		lbl << "a";
+		for_each(crts.begin(), crts.end(), [&](const string* s) { lbl << *s; });
+		lbl << "b";
+
+		cert = lbl.str();
+	}
 
 protected:
 	size_t _id;
