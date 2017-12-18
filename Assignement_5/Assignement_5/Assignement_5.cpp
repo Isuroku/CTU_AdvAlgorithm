f19dcef942c16d9c2ebe64dd254fe0702d619f38
Andrey Ekimov <ekimov@quantgames.com>
2017-12-18 10:46:31 +0300

    5 задание, слишком долго считает

---
+++ Assignement_5/Assignement_5/Assignement_5.cpp
@@ -0,0 +1,162 @@
+// Assignement_5.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "IOSwitcher.h"
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <map>
+
+const string arr_file_names[] =
+{
+	"Pd/pub01.in", //4 7
+	"Pd/pub02.in", //2 3
+	"Pd/pub03.in", //3 8
+	"Pd/pub04.in", //5 13
+	"Pd/pub05.in", //4 23
+	"Pd/pub06.in", //6 9
+	"Pd/pub07.in", //20 94
+	"Pd/pub08.in", //11 15
+	"Pd/pub09.in", //62 125
+	"Pd/pub10.in", //13 13
+	"Pd/pub11.in", //47 58
+	"Pd/pub12.in", //152 209
+};
+
+int levenshtein_distance(const string& src, const string& dst, const size_t dst_len)
+{
+	const size_t m = src.size();
+	const size_t n = min(dst_len, dst.size());
+	if (m == 0)
+		return n;
+	if (n == 0)
+		return m;
+
+	vector<vector<size_t>> matrix(m + 1);
+
+	for (size_t i = 0; i <= m; ++i) 
+	{
+		matrix[i].resize(n + 1);
+		matrix[i][0] = i;
+	}
+	for (size_t i = 0; i <= n; ++i)
+		matrix[0][i] = i;
+
+	size_t above_cell, left_cell, diagonal_cell, cost;
+
+	for (size_t i = 1; i <= m; ++i) 
+	{
+		for (size_t j = 1; j <= n; ++j) 
+		{
+			cost = src[i - 1] == dst[j - 1] ? 0 : 1;
+			above_cell = matrix[i - 1][j];
+			left_cell = matrix[i][j - 1];
+			diagonal_cell = matrix[i - 1][j - 1];
+			matrix[i][j] = min(min(above_cell + 1, left_cell + 1), diagonal_cell + cost);
+		}
+	}
+
+	return matrix[m][n];
+}
+
+map<string, int> _cache;
+
+int find_max_length_train2(const string& pattern, const int change_count, const string& formation)
+{
+	if (formation.size() == 0)
+		return 0;
+
+	if(_cache.count(formation) > 0)
+		return _cache[formation];
+
+	int max_value = INT_MAX - 1;
+
+	for(size_t i = 1; i <= formation.size(); ++i)
+	{
+		const int d = levenshtein_distance(pattern, formation, i);
+		if(d <= change_count)
+		{
+			const string sunstring = formation.substr(i, formation.size() - i);
+			const int child_res = find_max_length_train2(pattern, change_count, sunstring);
+			if (child_res < max_value)
+				max_value = child_res;
+		}
+	}
+
+	const int res = max_value + 1;
+	_cache[formation] = res;
+	return res;
+}
+
+int find_min_length_train2(const string& pattern, const int change_count, const string& formation)
+{
+	if (formation.size() == 0)
+		return 0;
+
+	if (_cache.count(formation) > 0)
+		return _cache[formation];
+
+	int max_value = INT_MIN + 1;
+	
+	for (size_t i = 1; i <= formation.size(); ++i)
+	{
+		const int d = levenshtein_distance(pattern, formation, i);
+		if (d <= change_count)
+		{
+			const int child_res = find_min_length_train2(pattern, change_count, formation.substr(i, formation.size() - i));
+			if (child_res > max_value)
+				max_value = child_res;
+		}
+	}
+
+	const int res = max_value + 1;
+	_cache[formation] = res;
+	return res;
+}
+
+//void test(const string& pattern)
+//{
+//	//lstnnt - pattern
+//	//tnt lsdtnttntl sdlsdtnt
+//	const string t = "lsdtnttntl";
+//	int d = levenshtein_distance(pattern, t, t.size());
+//}
+
+int main()
+{
+	string fn = "";
+	const int test_n = 8;
+	const bool from_file = test_n >= 0 && test_n < static_cast<int>(arr_file_names->length());
+	if (from_file)
+		fn = arr_file_names[test_n];
+
+	CIOSwitcher IOSwitcher(from_file, fn);
+
+	string formation;
+	IOSwitcher.getline(formation);
+
+	string pattern;
+	IOSwitcher.getline(pattern);
+
+	string s_change_count;
+	IOSwitcher.getline(s_change_count);
+	int change_count;
+	if (sscanf(s_change_count.c_str(), "%i", &change_count) != 1)
+	{
+		cerr << "read change_count was wrong";
+		return NULL;
+	}
+
+	//test(pattern);
+
+	const int max_length_train = find_max_length_train2(pattern, change_count, formation);
+
+	_cache.clear();
+	const int min_length_train = find_min_length_train2(pattern, change_count, formation);
+
+	cout << max_length_train << " " << min_length_train;
+
+    return 0;
+}
+
